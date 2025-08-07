e6ca2951fe17e93d57aeb62e73318e72e200ada4
arashid02-n <rashidnzri@gmail.com>
2025-08-07 23:30:37 +0330

    connect gmail with netlify

---
+++ netlify/function/send-message.js
@@ -0,0 +1,41 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+    };
+  }
+
+  const data = JSON.parse(event.body);
+  const { name, email, subject, message } = JSON.parse(event.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "rashidnzri@gmail.com",
+      pass: "rtgm hzxb ykor yczn", // ← رمز app password جیمیل اینجا بذار
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+  from: email,
+  to: "rashidnzri@gmail.com",
+  subject: subject || `Message from ${name}`,  // ← استفاده از subject ارسالی
+  text: message,
+});
+
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
